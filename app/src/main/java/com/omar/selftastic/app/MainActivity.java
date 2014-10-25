c317897d64182abe6ec8da788a3970048081568a
umarfarooq360 <omar.farooq@richmond.edu>
2014-10-25 18:06:23 -0400

    hahahhahha_2

--- app/src/main/java/com/omar/selftastic/app/MainActivity.java
+++ app/src/main/java/com/omar/selftastic/app/MainActivity.java
@@ -1,55 +1,106 @@
 package com.omar.selftastic.app;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 
-
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQ = 0;
     private Uri fileUri;
-
+     ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        img = (ImageView)findViewById(R.id.photo_image);
+        img.setImageDrawable(null);
 
-        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-        //fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
-        i.putExtra(MediaStore.EXTRA_OUTPUT , fileUri);
-
-        startActivityForResult(i, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        Button callCameraButton = (Button) findViewById(R.id.button_callcamera);
+        callCameraButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                File file = getOutputPhotoFile();
+                fileUri = Uri.fromFile(getOutputPhotoFile());
+                i.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+                startActivityForResult(i, CAPTURE_IMAGE_ACTIVITY_REQ);
+            }
+        });
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+    private File getOutputPhotoFile() {
+        File directory = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), getPackageName());
+        if (!directory.exists()) {
+            if (!directory.mkdirs()) {
+                Log.e("Baaahhhh", "Failed to create storage directory.");
+                return null;
+            }
         }
-        return super.onOptionsItemSelected(item);
+        String timeStamp = new SimpleDateFormat("yyyMMdd_HHmmss").format(new Date());
+        return new File(directory.getPath() + File.separator + "IMG_"
+                + timeStamp + ".jpg");
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQ) {
+            if (resultCode == RESULT_OK) {
+                Uri photoUri = null;
+                if (data == null) {
+                    // A known bug here! The image should have saved in fileUri
+                    Toast.makeText(this, "Image saved successfully",
+                            Toast.LENGTH_LONG).show();
+                    photoUri = fileUri;
+                } else {
+                    photoUri = data.getData();
+                    Toast.makeText(this, "Image saved successfully in: " + data.getData(),
+                            Toast.LENGTH_LONG).show();
+                }
+                showPhoto(photoUri);
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Callout for image capture failed!",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void showPhoto(Uri photoUri) {
+        File imageFile = new File(photoUri.getPath());
+        if (imageFile.exists()){
+            Bitmap bitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
+            BitmapDrawable drawable = new BitmapDrawable(this.getResources(), bitmap);
+            img.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            img.setImageDrawable(drawable);
+        }
+    }
+
+
+
+
+
+
 
 }
